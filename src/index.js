dfb68887a6f52a4a6cdc94e6caa9b41b43aa8294
Michael Boreham <bor3ham@gmail.com>
2020-07-17 12:44:14 +1000

    Add scaling tranformation

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDom from 'react-dom'
 import trilateration from 'node-trilateration'
 
 const CANVAS_WIDTH = 1000
-const CANVAS_HEIGHT = 800
+const CANVAS_HEIGHT = 600
 
 function TrilaterationSandbox(props) {
   const canvasRef = useRef(null)
@@ -13,6 +13,10 @@ function TrilaterationSandbox(props) {
   const [beaconB, setBeaconB] = useState({x: CANVAS_WIDTH * 0.7, y: CANVAS_HEIGHT * 0.25})
   const [beaconC, setBeaconC] = useState({x: CANVAS_WIDTH * 0.4, y: CANVAS_HEIGHT * 0.8})
 
+  const [xScale, setXScale] = useState(1)
+  const [yScale, setYScale] = useState(1)
+  const [uniformScaleAdjustment, setUniformScaleAdjustment] = useState(true)
+
   const [clicking, setClicking] = useState(false)
 
   const redraw = () => {
@@ -20,6 +24,19 @@ function TrilaterationSandbox(props) {
       return
     }
     const context = canvasRef.current.getContext('2d')
+
+    const transform = (loc) => {
+      return {
+        x: loc.x * xScale,
+        y: loc.y * yScale,
+      }
+    }
+    const untransform = (loc) => {
+      return {
+        x: loc.x / xScale,
+        y: loc.y / yScale,
+      }
+    }
 
     // fill background white
     context.beginPath()
@@ -49,11 +66,31 @@ function TrilaterationSandbox(props) {
     const distance = (loc1, loc2) => {
       return Math.pow(Math.pow(loc1.x - loc2.x, 2) + Math.pow(loc1.y - loc2.y, 2), 0.5)
     }
-    const result = trilateration.calculate([
-      {...beaconA, distance: distance(testSpot, beaconA)},
-      {...beaconB, distance: distance(testSpot, beaconB)},
-      {...beaconC, distance: distance(testSpot, beaconC)},
-    ])
+    const transformedBeaconA = transform(beaconA)
+    const transformedBeaconB = transform(beaconB)
+    const transformedBeaconC = transform(beaconC)
+    let beaconADistance = distance(testSpot, beaconA)
+    let beaconBDistance = distance(testSpot, beaconB)
+    let beaconCDistance = distance(testSpot, beaconC)
+    if (uniformScaleAdjustment) {
+      const beaconsScale = (bec1, bec2, bec3) => {
+        const dist12 = distance(bec1, bec2)
+        const dist13 = distance(bec1, bec3)
+        const dist23 = distance(bec2, bec3)
+        return (dist12 + dist13 + dist23) / 3
+      }
+      const originalScale = beaconsScale(beaconA, beaconB, beaconC)
+      const transformedScale = beaconsScale(transformedBeaconA, transformedBeaconB, transformedBeaconC)
+      const scaleAdjustment = originalScale / transformedScale
+      beaconADistance /= scaleAdjustment
+      beaconBDistance /= scaleAdjustment
+      beaconCDistance /= scaleAdjustment
+    }
+    const result = untransform(trilateration.calculate([
+      {...transformedBeaconA, distance: beaconADistance},
+      {...transformedBeaconB, distance: beaconBDistance},
+      {...transformedBeaconC, distance: beaconCDistance},
+    ]))
     // draw result spot
     context.beginPath()
     context.arc(result.x, result.y, 5, 0, 2 * Math.PI)
@@ -83,6 +120,15 @@ function TrilaterationSandbox(props) {
   const handleDocumentMouseUp = (event) => {
     setClicking(false)
   }
+  const handleXScaleChange = (event) => {
+    setXScale(event.target.value)
+  }
+  const handleYScaleChange = (event) => {
+    setYScale(event.target.value)
+  }
+  const handleUniformScaleAdjustmentChange = (event) => {
+    setUniformScaleAdjustment(event.target.checked)
+  }
 
   // document mouse listener
   useEffect(() => {
@@ -97,6 +143,9 @@ function TrilaterationSandbox(props) {
   }, [
     testSpot.x,
     testSpot.y,
+    xScale,
+    yScale,
+    uniformScaleAdjustment,
   ])
 
   return (
@@ -104,6 +153,46 @@ function TrilaterationSandbox(props) {
       <h1>Trilateration Sandbox</h1>
       <p>Welcome to the trilateration sandbox</p>
       <p>Test location: ({testSpot.x}, {testSpot.y})</p>
+      <h2>Transformation</h2>
+      <p>
+        Identify inaccuracies by adjusting the following transformation, which is applied to the
+        trilateration algorithm's beacon locations then reversed to draw relative to the original
+        test spot.
+      </p>
+      <div>
+        <label>X Scale</label>
+        <input
+          type="range"
+          min={0.1}
+          max={5}
+          step={0.1}
+          value={xScale}
+          onChange={handleXScaleChange}
+        />
+        <input type="text" value={xScale} onChange={handleXScaleChange} />
+      </div>
+      <div>
+        <label>Y Scale</label>
+        <input
+          type="range"
+          min={0.1}
+          max={5}
+          step={0.1}
+          value={yScale}
+          onChange={handleYScaleChange}
+        />
+        <input type="text" value={yScale} onChange={handleYScaleChange} />
+      </div>
+      <div>
+        <label>
+          <input
+            type="checkbox"
+            checked={uniformScaleAdjustment}
+            onChange={handleUniformScaleAdjustmentChange}
+          />
+          Uniform Scale Adjustment
+        </label>
+      </div>
       <canvas
         ref={canvasRef}
         width={CANVAS_WIDTH}
